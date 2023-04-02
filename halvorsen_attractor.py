a3906c76144d9f4b8b33cd409c722ecf2db8fd80
Dan Joar Heimvik <79584057+danj98@users.noreply.github.com>
2023-04-02 19:13:58 +0200

    Added halvorsen

---
+++ halvorsen_attractor.py
@@ -0,0 +1,62 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from scipy.integrate import solve_ivp
+
+# Random seed
+np.random.seed(1)
+# Number of points
+n = 1000
+# Create initial point
+x0, y0, z0 = (1, 0, 0)
+
+a = 1.4
+tmax = 1000
+
+
+# A single step for a Halvorsen attractor
+def halvorsen(t, coords, a):
+    x, y, z = coords
+    x_new = -a * x - 4 * y - 4 * z - y ** 2
+    y_new = -a * y - 4 * z - 4 * x - z ** 2
+    z_new = -a * z - 4 * x - 4 * y - x ** 2
+
+    return x_new, y_new, z_new
+
+
+soln = solve_ivp(halvorsen, (0, tmax), (x0, y0, z0), args=(a,), dense_output=True)
+interpolation = np.linspace(0, 1000, n)
+
+x_upd = np.zeros(len(interpolation))
+y_upd = np.zeros(len(interpolation))
+z_upd = np.zeros(len(interpolation))
+
+# Create 3d scatter plot
+fig = plt.figure()
+ax = fig.add_subplot(projection="3d")
+
+ax.xaxis.pane.fill = False
+ax.yaxis.pane.fill = False
+
+
+# Function to update each coordinate every frame
+def update_coordinates(i):
+    x_upd[i] = soln.sol(interpolation).T[i][0]
+    y_upd[i] = soln.sol(interpolation).T[i][1]
+    z_upd[i] = soln.sol(interpolation).T[i][2]
+    progress = (i / n) * 100
+    print("Progress: {:.1f}%".format(progress))
+    ax.clear()
+    ax.plot(x_upd[:i], y_upd[:i], z_upd[:i])
+
+
+# Setting axis properties
+ax.set(xlim3d=(-10, 10), xlabel="X")
+ax.set(ylim3d=(-10, 10), ylabel="Y")
+ax.set(zlim3d=(-10, 10), zlabel="Z")
+
+
+# Creating animation
+ani = FuncAnimation(fig, update_coordinates, np.arange(n), interval=20, repeat=False)
+ani.save("output/halvorsen.gif", dpi=300, writer="pillow", fps=50)
+print("Done!")
